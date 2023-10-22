901ce1eb1730885ad44579eae2c004099d85c41b
tanayparikh123 <67798681+tanayparikh123@users.noreply.github.com>
2023-10-21 23:32:05 -0400

    Update UseCase.py

--- UseCase.py
+++ UseCase.py
@@ -60,3 +60,63 @@ def get_actual_data():
     SELECT day AS ts, impression_count AS actual
     FROM daily_impressions;
     """
+    # Fetch the data into a Pandas DataFrame
+    df = pd.read_sql(query, conn)
+
+    # Close the Snowflake connection
+    conn.close()
+
+    return df
+
+# Function to check for an anomaly
+def check_for_anomaly(date, impressions):
+    # Connect to Snowflake
+    conn = connect(
+        user=snowflake_username,
+        password=snowflake_password,
+        account=snowflake_account,
+        database=snowflake_database
+    )
+
+    cursor = conn.cursor()
+
+    # Set a current schema (replace 'your_schema_name' with the actual schema name)
+    schema_name = 'DEMO'
+    set_schema_query = f"USE SCHEMA {schema_name}"
+    cursor.execute(set_schema_query)
+
+    # Create a temporary table to hold the input data
+    create_temp_table_query = f"""
+    CREATE OR REPLACE TEMPORARY TABLE temp_input_data (
+        day TIMESTAMP_NTZ(9),
+        impressions NUMBER(5, 0)
+    );
+    """
+    cursor.execute(create_temp_table_query)
+
+    # Insert data into the temporary table
+    insert_data_query = f"""
+    INSERT INTO temp_input_data (day, impressions) VALUES (TO_TIMESTAMP('{date}'), {impressions});
+    """
+    cursor.execute(insert_data_query)
+    
+    # Commit the transaction to ensure data is available
+    cursor.execute("COMMIT;")
+
+    # Execute the anomaly detection
+    query = f"""
+    CALL impression_anomaly_detector!DETECT_ANOMALIES(
+        INPUT_DATA => SYSTEM$QUERY_REFERENCE('SELECT * FROM temp_input_data'),
+        TIMESTAMP_COLNAME => 'day',
+        TARGET_COLNAME => 'impressions'
+    );
+    """
+    cursor.execute(query)
+    
+    result = cursor.fetchall()
+
+    # Close the Snowflake connection
+    conn.close()
+
+    return result
+
