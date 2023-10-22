e57e9ce16397b080b93a3cc7df06b612bfaa3f3e
tanayparikh123 <67798681+tanayparikh123@users.noreply.github.com>
2023-10-21 23:31:25 -0400

    Update UseCase.py

--- UseCase.py
+++ UseCase.py
@@ -23,3 +23,40 @@ def get_forecast_data():
         database=snowflake_database
     )
 
+
+    # Create the forecast model if it doesn't exist (or use your own script to create the model)
+
+    # Execute the forecast model
+    cursor = conn.cursor()
+    cursor.execute("CALL impressions_forecast!FORECAST(FORECASTING_PERIODS => 14);")
+    cursor.close()
+
+    # Execute a query to retrieve the forecast data
+    query = """
+    SELECT ts, NULL AS actual, forecast, lower_bound, upper_bound
+    FROM TABLE(RESULT_SCAN(-1));
+    """
+
+    # Fetch the data into a Pandas DataFrame
+    df = pd.read_sql(query, conn)
+
+    # Close the Snowflake connection
+    conn.close()
+
+    return df
+
+# Function to get actual data
+def get_actual_data():
+    # Connect to Snowflake
+    conn = connect(
+        user=snowflake_username,
+        password=snowflake_password,
+        account=snowflake_account,
+        database=snowflake_database
+    )
+
+    # Execute a query to retrieve the actual data
+    query = """
+    SELECT day AS ts, impression_count AS actual
+    FROM daily_impressions;
+    """
